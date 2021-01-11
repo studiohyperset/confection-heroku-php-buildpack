2bd383f1fb4e16f70a3e68188ef2f32015bcf515
Bruno Cantuaria <bruno@cantuaria.net.br>
2021-01-11 18:05:15 -0300

    Change

--- bin/snowflake.sh
+++ bin/snowflake.sh
@@ -28,10 +28,8 @@ echo "[LOG] Copying cacert to configuration folder"
 cp $snowflake_dir/libsnowflakeclient/cacert.pem /app/.heroku/php/etc/php/
 
 echo "[LOG] Creating snowflake.ini"
-cat "/app/.heroku/php/etc/php/20-pdo_snowflake.ini" 
-
 echo "[LOG] Inserting extension and certificate on snowflake.ini"
-echo "extension=pdo_snowflake.so" >> /app/.heroku/php/etc/php/20-pdo_snowflake.ini 
+echo "extension=pdo_snowflake.so" > /app/.heroku/php/etc/php/20-pdo_snowflake.ini 
 echo "pdo_snowflake.cacert=/app/.heroku/php/etc/php/cacert.pem" >> /app/.heroku/php/etc/php/20-pdo_snowflake.ini 
 
 echo "[LOG] Finished Snowflake Setup"
